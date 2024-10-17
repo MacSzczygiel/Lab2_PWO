68a55acd7cc54b5523dd2a828d64636c823d8d8a
spixxer <filipstrozek@gmail.com>
2024-10-17 09:03:30 +0200

    student dodany i wyświetlony

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Witamy studencie");
+        Student st = new Student("Adam","Piotrowicki",20);
+
+
+        System.out.println("Student: " + st);
     }
 }
