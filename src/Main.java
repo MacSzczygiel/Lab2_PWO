7b404db7c37b7d90793143320c6fc4d00efead29
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-10-17 09:08:01 +0200

    init factorial calculator
    zmiana 5->6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Integer number = 6; // Example number
+        int result = FactorialCalculator.calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
     }
 }
