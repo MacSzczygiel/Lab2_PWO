583b47082fcdee2f7e27f174ff7b5979eb90cc63
Mac <maciooox3@gmail.com>
2024-10-17 09:06:52 +0200

    squash
    line 5 addded
    
    line 6 addded
    
    fix/updated souts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Witam");
+        System.out.println("Hola");
+        System.out.println("Dzień dobry");
     }
 }
